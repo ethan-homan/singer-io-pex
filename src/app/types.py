ce680140e87acdb9988b53655283a340c775be34
ethan-homan <ewhoman@bu.edu>
2022-05-13 16:38:19 -0400

    remove app

--- src/app/types.py
+++
@@ -1,9 +0,0 @@
-import datetime
-from pydantic import BaseModel, Field
-
-
-class GitHubSyncConfig(BaseModel):
-    access_token: str = Field(..., description="GitHub personal access token.")
-    repository: str = Field(..., description="Repository in the format 'owner-name/repo-name.")
-    start_date: datetime.datetime = Field(..., description="The date to start looking for records at.")
-    request_timeout: int = 300
